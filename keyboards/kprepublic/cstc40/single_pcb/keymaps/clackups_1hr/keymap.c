dfe723add51882c236a03de46ea6632562848763
clackups <clackups@gmail.com>
2024-08-24 21:46:42 +0200

    1-hand right layout for Planck: work in progress

--- keyboards/kprepublic/cstc40/single_pcb/keymaps/clackups_1hr/keymap.c
+++ keyboards/kprepublic/cstc40/single_pcb/keymaps/clackups_1hr/keymap.c
@@ -1,6 +1,8 @@
 /*
   Mirrored keyboard for one-hand typing (right hand).
   Author: clacups
+
+  ALT, SHIFT and CTRL are sticky keys.
  */
 
 #include QMK_KEYBOARD_H
@@ -9,37 +11,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Primary layer
  * ,-----------------------------------------------------------------------------------.
- * |      |      |      |      |   Y  |   U  |   I  |   O  |   P  |   {  |   }  | Bksp |
+ * |   ~  |   7  |   8  |   9  |   Y  |   U  |   I  |   O  |   P  |   {  |   }  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |   H  |   J  |   K  |   L  |   ;  |  "   | Ctrl | Esc  |
+ * |      |   4  |   5  |   6  |   H  |   J  |   K  |   L  |   ;  |  "   | Ctrl | Esc  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |   N  |   M  |   ,  |  .   |   /  | Enter|  Up  | Tab  |
+ * |      |   1  |   2  |   3  |   N  |   M  |   ,  |  .   |   /  | Enter|  Up  | Tab  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      | Space|    Space    |  Alt |Shift |Left  | Down |Right |
+ * |      |      |   0  |      | Space|    Space    |  Alt |Shift |Left  | Down |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [0] = LAYOUT_planck_mit(
-    _______,   _______,     _______,     _______,     KC_Y,    KC_U,     KC_I,     KC_O,     KC_P,     KC_LCBR, KC_RCBR, KC_BSPC,
-    _______,   _______,     _______,     _______,     KC_H,    KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT, KC_RCTL, KC_ESC,
-    _______,   _______,     _______,     _______,     KC_N,    KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT,  KC_UP,   KC_TAB,
-    _______,   _______,     _______,     _______,     LT(1,KC_SPC),  LT(1,KC_SPC), KC_RALT,  KC_RSFT,  KC_LEFT,  KC_DOWN, KC_RGHT
+    KC_TILD,      KC_7,        KC_8,        KC_9,     KC_Y,    KC_U,     KC_I,     KC_O,     KC_P,     KC_LCBR, KC_RCBR, KC_BSPC,
+    _______,      KC_4,        KC_5,        KC_6,     KC_H,    KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT, OSM(MOD_RCTL), KC_ESC,
+    _______,      KC_1,        KC_2,        KC_3,     KC_N,    KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT,  KC_UP,   KC_TAB,
+    _______,   _______,        KC_0,     _______,     LT(1,KC_SPC),  LT(1,KC_SPC),OSM(MOD_RALT),OSM(MOD_RSFT),KC_LEFT,  KC_DOWN, KC_RGHT
 ),
 
 /* Secondary layer, activated by holding the space key
  * ,-----------------------------------------------------------------------------------.
- * |      |      |      |      |   T  |   R  |   E  |   W  |   Q  |      |      |      |
+ * |      |  F7  |  F8  |  F9  |   T  |   R  |   E  |   W  |   Q  |   =  |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |   G  |   F  |   D  |   S  |   A  |      |      |      |
+ * |      |  F4  |  F5  |  F6  |   G  |   F  |   D  |   S  |   A  |   -  |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |   B  |   V  |   C  |   X  |   Z  |      |      |   \  |
+ * |      |  F1  |  F2  |  F3  |   B  |   V  |   C  |   X  |   Z  |      |      |   \  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [1] = LAYOUT_planck_mit(
-    _______,   _______,     _______,     _______,     KC_T,    KC_R,     KC_E,     KC_W,     KC_Q,     _______, _______, _______,
-    _______,   _______,     _______,     _______,     KC_G,    KC_F,     KC_D,     KC_S,     KC_A,     _______, _______, _______,
-    _______,   _______,     _______,     _______,     KC_B,    KC_V,     KC_C,     KC_X,     KC_Z,     _______, _______, KC_BACKSLASH,
+    _______,     KC_F7,       KC_F8,       KC_F9,     KC_T,    KC_R,     KC_E,     KC_W,     KC_Q,      KC_EQL, _______, _______,
+    _______,     KC_F4,       KC_F5,       KC_F6,     KC_G,    KC_F,     KC_D,     KC_S,     KC_A,     KC_MINS, _______, _______,
+    _______,     KC_F1,       KC_F2,       KC_F3,     KC_B,    KC_V,     KC_C,     KC_X,     KC_Z,     _______, _______, KC_BACKSLASH,
     _______,   _______,     _______,     _______,   _______,  _______,            _______,  _______,   _______, _______, _______
 ),
 
